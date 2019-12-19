bfd337367f61429e2a26565ada7ebead5e28fecb
BaoHaoYu <a450215151@outlook.com>
2019-12-19 08:15:05 +0800

    m: 简化

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,6 @@ import App from './App/App.vue'
 
 Vue.config.productionTip = false
 
-function renderApp() {
-  new Vue({
-    render: (h) => h(App),
-  }).$mount('#root')
-}
-
-renderApp()
+new Vue({
+  render: (h) => h(App),
+}).$mount('#root')
